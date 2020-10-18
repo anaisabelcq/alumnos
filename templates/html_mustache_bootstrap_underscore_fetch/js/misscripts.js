5827b0322930c3035dc94f19016dd469591322d9
andy <arteysoft@gmail.com>
2020-10-18 16:20:23 -0300

    modifica misscripts

--- templates/html_mustache_bootstrap_underscore_fetch/js/misscripts.js
+++ templates/html_mustache_bootstrap_underscore_fetch/js/misscripts.js
@@ -1,11 +1,52 @@
-function ejemploFetchGet() {
-    
+function ejemploGet() {
+    fetch('mi/getty')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log('data = ', data);
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
 }
 
-function ejemploFetchPost() {
-
+function ejemploPostFormulario() {
+    fetch('/mi', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'a=1&b=2'
+        })
+        .then(function(response) {
+            console.log('response =', response);
+            return response.json();
+        })
+        .then(function(data) {
+            console.log('data = ', data);
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
 }
 
-_.delay(()=>{
-    $(document).children().detach();
-}, 3000);
+function ejemploPostObject() {
+    fetch('/mi', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({nombre:'aldo', apellido:'sierra'})
+        })
+        .then(function(response) {
+            console.log('response =', response);
+            return response.json();
+        })
+        .then(function(data) {
+            console.log('data = ', data);
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
+}
